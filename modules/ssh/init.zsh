c3cbec61182663b166a6eafd293eaebd969ece1f
tduzan <tyler.duzan@solidfire.com>
2015-07-07 17:30:57 -0600

    Had to add an additional check to ensure ssh-agent gets started even though launchctl is watching the socket when ZSH is executed.

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -28,6 +28,14 @@ if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
     eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
   fi
+elif [[ "$SSH_AUTH_SOCK" == "$(launchctl getenv SSH_AUTH_SOCK)" ]]; then
+    # Export environment variables.
+    source "$_ssh_agent_env" 2> /dev/null
+
+    # Start ssh-agent if not started.
+    if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
+      eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+    fi
 fi
 
 # Create a persistent SSH authentication socket.
