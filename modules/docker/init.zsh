d1316c2ff8ba04aec9a8aa9d40e73ce7a8ec7b5a
Phil Toland <phil.toland@gmail.com>
2015-12-29 21:03:49 -0600

    Fix the docker module when b2d isn't installed
    1Check for the existance of boot2docker rather than assuming it exists on OSX. boot2docker is deprecated in favor of docker machine, so the docker aliases are useful even if b2d isn't installed.

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -5,20 +5,18 @@
 #   andrew williams (https://github.com/skarfacegc)
 #
 
-# Only setup the boot2docker commands and aliases if we're on OSX
-if [[ "$OSTYPE" == darwin* ]]; then
+# Only setup the boot2docker commands and aliases if it is installed
+if (( $+commands[boot2docker] )); then
+ # Set environment if boot2docker is already running
+ if [[ $(boot2docker status) == "running" ]]; then
+   eval $(boot2docker shellinit 2> /dev/null)
+ fi
 
-    # Set environment if boot2docker is already running
-    if [[ $(boot2docker status) == "running" ]]; then
-        eval $(boot2docker shellinit 2> /dev/null)
-    fi
-    
-    alias b2d='boot2docker'
-    alias b2dstart='boot2docker start &> /dev/null && eval $(boot2docker shellinit 2> /dev/null)'
-    alias b2dstop='boot2docker stop'
-    alias b2dssh='boot2docker ssh'
-    alias b2di='boot2docker info'
-    
+ alias b2d='boot2docker'
+ alias b2dstart='boot2docker start &> /dev/null && eval $(boot2docker shellinit 2> /dev/null)'
+ alias b2dstop='boot2docker stop'
+ alias b2dssh='boot2docker ssh'
+ alias b2di='boot2docker info'
 fi
 
 
@@ -32,4 +30,3 @@ docbash() {
     docker exec -t -i "$1" /bin/bash
 }
 compdef -e 'words[1]=(docker exec); service=docker; (( CURRENT+=1 )); _docker' docbash
-
